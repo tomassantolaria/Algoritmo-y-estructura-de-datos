98fc2b8513e23bb64dc87349608b0c973bff09d4
tomassantolaria <83884536+tomassantolaria@users.noreply.github.com>
2023-06-06 21:08:18 -0300

    Update main.cpp

--- TP1/Ejercicio n°3a/main.cpp
+++ TP1/Ejercicio n°3a/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    float valor ;
+    int valor ;
     cout << "Ingrese un valor numerico para calcular en mts/seg" << endl;
     cin >> valor ;
     cout << "El valor ingresado transfromado en mts/seg es:" << (valor)*(0.2777777778) << endl;
